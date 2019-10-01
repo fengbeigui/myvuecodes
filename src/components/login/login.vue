4be00be75dda58e7a3042324a2aec6d3da47b32a
fengbeigui <fbg0618@163.com>
2019-10-02 00:43:22 +0800

    0.5.验证登录完成

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="login-wrap">
-    <el-form ref="form" :label-position="labelPosition" label-width="100px" class="login-from">
+      <!-- :model="form" 绑定元素数据 -->
+      <!-- :rules="rules" 设置验证规则 -->
+    <el-form
+      ref="form"
+      :rules="rules"
+      :model="form"
+      :label-position="labelPosition"
+      label-width="100px"
+      class="login-from"
+    >
       <h2>用户登录</h2>
-      <el-form-item label="用户名" prop="pass">
-        <el-input type="text" v-model="username"></el-input>
+      <el-form-item label="用户名" prop="username">
+        <el-input type="text" v-model="form.username"></el-input>
       </el-form-item>
-      <el-form-item label-position="top" label="密码">
-        <el-input type="password" v-model="password"></el-input>
+      <el-form-item label-position="top" label="密码" prop="password">
+        <el-input type="password" v-model="form.password"></el-input>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="onSubmit" class="login-btn">登录</el-button>
@@ -21,24 +30,49 @@ export default {
     return {
       //绑定的属性储存
       labelPosition: "top",
-      username: "",
-      password: ""
+      form: {
+        username: "",
+        password: ""
+      },
+
+      //保存一个对象,这是验证规则
+      rules: {
+          //验证用户名，使用prop
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" }
+        ],
+        password: [
+            { required: true, message: "请输入密码", trigger: "blur" },
+          {
+            required: true,
+            min: 3,
+            max: 8,
+            message: "长度在 3 到 8 个数字",
+            trigger: "blur"
+          }
+        ]
+      }
     };
   },
   methods: {
     //事件属性onSubmit,登录按钮事件
     onSubmit() {
+        this.$refs.form.validate(res=>{
+          //如果有错误信息  直接停止代码执行
+          if(res){
+              return;
+          }
+      })
       // console.log(this.username,this.userpwd);
 
       // 使用axios自己封装的axios的$myHttp,发送post 请求，传入data中的数据,注意写法
       this.$myHttp
-        .post("http://localhost:8888/api/private/v1/login", {
-          username: this.username,
-          password: this.password
-        })
+        .post("http://localhost:8888/api/private/v1/login", 
+        this.form,
+        )
         .then(back_data => {
           console.log(back_data);
-          var {data,meta} = back_data.data;
+          var { data, meta } = back_data.data;
           //判断  如果登录成功，做跳转
           if (meta.status == 200) {
             //element插件属性
