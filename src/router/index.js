332d5b54daa0c697e31d419193246a5b89c30d21
fengbeigui <fbg0618@163.com>
2019-10-01 20:38:01 +0800

    1.5.登陆路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+//导入登录
+import Login from '@/components/login/login'
 
 Vue.use(Router)
 
@@ -10,6 +12,10 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/login',
+      component: Login
     }
   ]
 })
